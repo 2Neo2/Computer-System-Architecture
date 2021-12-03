185587979ece336ecbab1ed8e2a6f9d189324324
2Neo2 <64828077+2Neo2@users.noreply.github.com>
2021-12-03 14:26:44 +0300

    Add files via upload

---
+++ AVSHomework1/CMakeFiles/AVSHomework1.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/ivandoronin/Downloads/Computer-System-Architecture-main/AVSHomework1/Plants/Files-with-cpp/container.cpp" "CMakeFiles/AVSHomework1.dir/Plants/Files-with-cpp/container.cpp.o" "gcc" "CMakeFiles/AVSHomework1.dir/Plants/Files-with-cpp/container.cpp.o.d"
+  "/Users/ivandoronin/Downloads/Computer-System-Architecture-main/AVSHomework1/Plants/Files-with-cpp/flower.cpp" "CMakeFiles/AVSHomework1.dir/Plants/Files-with-cpp/flower.cpp.o" "gcc" "CMakeFiles/AVSHomework1.dir/Plants/Files-with-cpp/flower.cpp.o.d"
+  "/Users/ivandoronin/Downloads/Computer-System-Architecture-main/AVSHomework1/Plants/Files-with-cpp/plant.cpp" "CMakeFiles/AVSHomework1.dir/Plants/Files-with-cpp/plant.cpp.o" "gcc" "CMakeFiles/AVSHomework1.dir/Plants/Files-with-cpp/plant.cpp.o.d"
+  "/Users/ivandoronin/Downloads/Computer-System-Architecture-main/AVSHomework1/Plants/Files-with-cpp/randomGenerator.cpp" "CMakeFiles/AVSHomework1.dir/Plants/Files-with-cpp/randomGenerator.cpp.o" "gcc" "CMakeFiles/AVSHomework1.dir/Plants/Files-with-cpp/randomGenerator.cpp.o.d"
+  "/Users/ivandoronin/Downloads/Computer-System-Architecture-main/AVSHomework1/Plants/Files-with-cpp/shrub.cpp" "CMakeFiles/AVSHomework1.dir/Plants/Files-with-cpp/shrub.cpp.o" "gcc" "CMakeFiles/AVSHomework1.dir/Plants/Files-with-cpp/shrub.cpp.o.d"
+  "/Users/ivandoronin/Downloads/Computer-System-Architecture-main/AVSHomework1/Plants/Files-with-cpp/tree.cpp" "CMakeFiles/AVSHomework1.dir/Plants/Files-with-cpp/tree.cpp.o" "gcc" "CMakeFiles/AVSHomework1.dir/Plants/Files-with-cpp/tree.cpp.o.d"
+  "/Users/ivandoronin/Downloads/Computer-System-Architecture-main/AVSHomework1/main.cpp" "CMakeFiles/AVSHomework1.dir/main.cpp.o" "gcc" "CMakeFiles/AVSHomework1.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ AVSHomework1/CMakeFiles/AVSHomework1.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "AVSHomework1"
+  "AVSHomework1.pdb"
+  "CMakeFiles/AVSHomework1.dir/Plants/Files-with-cpp/container.cpp.o"
+  "CMakeFiles/AVSHomework1.dir/Plants/Files-with-cpp/container.cpp.o.d"
+  "CMakeFiles/AVSHomework1.dir/Plants/Files-with-cpp/flower.cpp.o"
+  "CMakeFiles/AVSHomework1.dir/Plants/Files-with-cpp/flower.cpp.o.d"
+  "CMakeFiles/AVSHomework1.dir/Plants/Files-with-cpp/plant.cpp.o"
+  "CMakeFiles/AVSHomework1.dir/Plants/Files-with-cpp/plant.cpp.o.d"
+  "CMakeFiles/AVSHomework1.dir/Plants/Files-with-cpp/randomGenerator.cpp.o"
+  "CMakeFiles/AVSHomework1.dir/Plants/Files-with-cpp/randomGenerator.cpp.o.d"
+  "CMakeFiles/AVSHomework1.dir/Plants/Files-with-cpp/shrub.cpp.o"
+  "CMakeFiles/AVSHomework1.dir/Plants/Files-with-cpp/shrub.cpp.o.d"
+  "CMakeFiles/AVSHomework1.dir/Plants/Files-with-cpp/tree.cpp.o"
+  "CMakeFiles/AVSHomework1.dir/Plants/Files-with-cpp/tree.cpp.o.d"
+  "CMakeFiles/AVSHomework1.dir/main.cpp.o"
+  "CMakeFiles/AVSHomework1.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AVSHomework1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
